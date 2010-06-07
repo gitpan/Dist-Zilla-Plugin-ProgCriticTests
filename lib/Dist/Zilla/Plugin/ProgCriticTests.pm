3d3a516937fab35339d525a4b1a71a02c991446d
Christian Walde <walde.christian@gmail.com>
2010-06-07 15:22:12 +0000

    Import of MITHALDU/Dist-Zilla-Plugin-ProgCriticTests-1.101582 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ProgCriticTests
    gitpan-cpan-version:      1.101582
    gitpan-cpan-path:         MITHALDU/Dist-Zilla-Plugin-ProgCriticTests-1.101582.tar.gz
    gitpan-cpan-author:       MITHALDU
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/ProgCriticTests.pm
+++ lib/Dist/Zilla/Plugin/ProgCriticTests.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::ProgCriticTests;
 BEGIN {
-  $Dist::Zilla::Plugin::ProgCriticTests::VERSION = '1.101581';
+  $Dist::Zilla::Plugin::ProgCriticTests::VERSION = '1.101582';
 }
 # ABSTRACT: Gradually enforce coding standards with Dist::Zilla
 
@@ -58,7 +58,7 @@ Dist::Zilla::Plugin::ProgCriticTests - Gradually enforce coding standards with D
 
 =head1 VERSION
 
-version 1.101581
+version 1.101582
 
 =head1 SYNOPSIS
 
